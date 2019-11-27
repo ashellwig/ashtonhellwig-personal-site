cdd3f054214d64bf511668b922bb41b49f224a28
Ashton Hellwig <ahellwig.dev@gmail.com>
2019-11-26 23:55:56 -0700

    upgrade vue cli

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,20 +9,19 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'max-len': ['error', {
-      'code': 80
-    }]
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
   },
-  overrides: [{
-    files: [
-      '**/__tests__/*.{j,t}s?(x)'
-    ],
-    env: {
-      mocha: true
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)'
+      ],
+      env: {
+        mocha: true
+      }
     }
-  }]
+  ]
 }
