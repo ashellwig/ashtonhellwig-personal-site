5f6c86b784749545e6641845ee848e284adea9b6
Ashton Hellwig <ahellwig.dev@gmail.com>
2019-11-27 00:18:28 -0700

    add vuetify custom theme to plugin file

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,23 +2,37 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
+const darkTheme = {
+  primary: '#1B7872',
+  accent: '#F48FB1',
+  secondary: '#1B5678',
+  success: '#66BB6A',
+  info: '#2196F3',
+  warning: '#FB8C00',
+  error: '#C62828'
+}
+
+const lightTheme = {
+  primary: '#1976D2',
+  accent: '#e91e63',
+  secondary: '#30b1dc',
+  success: '#4CAF50',
+  info: '#2196F3',
+  warning: '#FB8C00',
+  error: '#FF5252'
+}
+
 Vue.use(Vuetify)
 
 export default new Vuetify({
   theme: {
+    dark: true,
+    themes: {
+      dark: darkTheme,
+      light: lightTheme
+    },
     options: {
       customProperties: true
-    },
-    themes: {
-      light: {
-        primary: '#ee44aa',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107'
-      }
     }
   },
   icons: {
